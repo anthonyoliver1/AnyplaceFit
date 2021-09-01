38327785c876bc37d99b26dfa756d092d2ffb6a1
anthonyoliver1 <anthony.silvaoliveira@outlook.com>
2021-09-01 19:46:47 -0300

    feat(anyplaceFit): new commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,10 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { AppHomeComponent } from './components/app-home/app-home.component';
+import { CardsComponent } from './components/cards/cards.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LoginComponent } from './components/login/login.component';
+import { NewAccountComponent } from './components/new-account/new-account.component';
 
 const routes: Routes = [
   {
@@ -11,6 +16,12 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  { path: 'login', component: LoginComponent },
+  { path: 'header', component: HeaderComponent },
+  { path: 'newAccount', component: NewAccountComponent },
+  { path: 'app-home', component: AppHomeComponent },
+  { path: 'cards', component: CardsComponent },
+
 ];
 
 @NgModule({
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
+import { HeaderComponent } from '../components/header/header.component';
+import { LoginComponent } from '../components/login/login.component';
+import { NewAccountComponent } from '../components/new-account/new-account.component';
+import { BrMaskerModule } from 'br-mask';
+import { CardsComponent } from '../components/cards/cards.component';
+import { AppHomeComponent } from '../components/app-home/app-home.component';
 
 
 @NgModule({
@@ -12,8 +18,17 @@ import { HomePageRoutingModule } from './home-routing.module';
     CommonModule,
     FormsModule,
     IonicModule,
-    HomePageRoutingModule
+    HomePageRoutingModule,
+    ReactiveFormsModule,
+    BrMaskerModule
   ],
-  declarations: [HomePage]
+  declarations: [
+    HomePage,
+    HeaderComponent,
+    LoginComponent,
+    NewAccountComponent,
+    CardsComponent,
+    AppHomeComponent
+  ],
 })
 export class HomePageModule {}
